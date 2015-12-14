bdd737dd5548dfe9e7786dcac46a57e30b9ac5c9
Adrian-Tudor Panescu <adrian@panescu.com>
2015-12-14 14:34:51 +0200

    client.js: comment

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router} from 'react-router';
 
+// note that by using this entry point we are requsing the server-side code
 import {routes} from './routes';
 
 
