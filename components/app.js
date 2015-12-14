19491d63b24f77fefdbf12d1bf2cd00466d72e51
Adrian-Tudor Panescu <adrian@panescu.com>
2015-12-14 12:11:36 +0200

    navigation

--- components/app.js
+++ components/app.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router';
 import React from 'react';
 
 export default class AppComponent extends React.Component {
@@ -5,6 +6,14 @@ export default class AppComponent extends React.Component {
         return (
             <div>
                 <h2>Welcome to my App</h2>
+
+                {/* navigation present on all pages */}
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                </ul>
+
+                {/* the actual content of the page */}
                 {this.props.children}
             </div>
         );
