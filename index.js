0eccf2809d50c1d004836adfee8026e93106d731
Adrian-Tudor Panescu <adrian@panescu.com>
2015-12-14 11:50:08 +0200

    working routes

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 import express from 'express';
 import http from 'http';
+import * as codes from 'http-codes';
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import {match, RoutingContext} from 'react-router';
+
+import AppComponent from './components/app.js';
+import IndexComponent from './components/index.js';
+
+
+const routes = {
+    'path': '',
+    'component': AppComponent,
+    'childRoutes': [
+        {
+            'path': '/',
+            'component': IndexComponent
+
+        }
+    ]
+};
 
 const app = express();
 
@@ -8,7 +28,21 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('*', (req, res) => {
-    res.render('index');
+    match({routes, 'location': req.url}, (err, redirect, props) => {
+        if (err) {
+            res.status(codes.INTERNAL_SERVER_ERROR).send(err.message);
+        } else if (redirect) {
+            res.redirect(codes.MOVED_TEMPORARILY,
+                `${redirect.pathname}${redirect.search}`
+            );
+        } else if (props) {
+            const markup = renderToString(<RoutingContext {...props} />);
+            res.render('index', {markup});
+        } else {
+            res.sendStatus(codes.NOT_FOUND);
+        }
+
+    });
 });
 
 const server = http.createServer(app);
